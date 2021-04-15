5279f47d3fa651dfa8365a6aa635acd6f9ff9e03
elPansi <fernandohc@gmail.com>
2021-04-15 14:54:06 +0000

    Ultimos cambios

--- helpdesk_fernandohernandez/models/helpdesk_ticket.py
+++ helpdesk_fernandohernandez/models/helpdesk_ticket.py
@@ -4,10 +4,25 @@ class HelpdeskTicket(models.Model):
     _name = 'helpdesk.ticket'
     _description = 'Ticket'
 
-    name = fields.Char(
-        string='Name')
-    description = fields.Text(
-        string='Description')
-    date = fields.Date(
-        string='Date'
-    )
+    name = fields.Char(string='Name', required = True)
+    description = fields.Text(string='Description')
+    date = fields.Date(string='Date')
+    state = fields.Selection(
+        [('nuevo', 'Nuevo'), #valor bd, valor en la vista
+        ('asignado', 'Asignado'),
+        ('proceso', 'En proceso'),
+        ('pendiente', 'Pendiente'),
+        ('resuelto', 'Resuelto'),
+        ('cancelado', 'Cancelado')],
+        string='State',
+        default='nuevo')
+
+    time = fields.Float(string='Time')
+    assigned = fields.Boolean(string='Assigned', readonly=True)
+    date_limit = fields.Date(string='Date Limit' )
+    action_corrective = fields.Html(
+        string="Corrective Action",
+        help='Descrive corrective actions to do')
+    action_preventive = fields.Html(
+        string="Preventive Action",
+        help='Descrive corrective actions to do')
